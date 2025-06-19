ffbad98b6dba8ebcf574b144cadbd1673a5af74b
chenjiawei30 <www.chenjiawei@foxmail.com>
2025-06-19 20:31:57 +0800

    Feat: 增加发帖和评论功能，修复一些已知问题

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -1,15 +1,19 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import staticfiles
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.urls import path, include
 from . import view
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 urlpatterns = [
     path('', view.travelHome),
     path('travelhome',view.travelHome),
     path('404.html',view.error404),
     path('about.html',view.about),
-    path('blog.html', view.blog),
+    path('blog.html', view.blog, name='blog'),
     path('blog-single.html', view.blog_single, name='blog_single'),
     path('contact.html', view.contact),
     path('gallery.html', view.gallery),
@@ -24,6 +28,16 @@ urlpatterns = [
     path('chat/', view.chat_view, name='chat'),
     path("seasonal/", view.seasonal_spot, name="seasonal_spot"),
     path('delete_comment/<int:comment_id>/', view.delete_comment, name='delete_comment'),
+    path('post_create/', view.post_create, name='post_create'),
+    path('delete_post/<int:post_id>/', view.delete_post, name='delete_post'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def blog_single(request):
+    post_id = request.GET.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog-single.html', {'post': post})
 
 
